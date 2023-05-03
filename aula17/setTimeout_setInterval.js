db124d280addba93da41d46e4c34a5189e6f9f77
ton-long <114124041+ton-long@users.noreply.github.com>
2023-05-03 03:57:58 -0300

    Create setTimeout_setInterval.js

---
+++ aula17/setTimeout_setInterval.js
@@ -0,0 +1,16 @@
+//setTimeout
+
+setTimeout(function() {
+
+    console.log('Testando o setTimeout');
+
+}, 2000);
+
+
+//setInterval
+
+setInterval(function(){
+
+    console.log('Testando setInterval');
+
+}, 1000)
